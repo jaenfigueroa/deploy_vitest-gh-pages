b1dbc3d92b79a96ec66c286c1044a2451423817d
Jaen Figueroa <jaenfigueroa03042000@gmail.com>
2024-01-20 08:29:22 -0500

    deploy

--- vite.config.sin-actions.ts
+++ vite.config.sin-actions.ts
@@ -1,8 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+/*
+VERSION SI USAR GITUB ACTIONS, PARA DESPLEGAR DESDE MI PROPIA TERMINAL
+*/
 
 // https://vitejs.dev/config/
 export default defineConfig({
   // base: '/deploy_vitest-gh-pages', /* ACTIVAR ESTA LINEA, solo cuando `pnpm run build` y luego `pnpm run deploy` */
   plugins: [react()],
-})
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // solo si tambien vamos a implementar el deplieuge desde github actions
-const isGitHubActions = process.env.GITHUB_ACTIONS === 'true'
+const isGitHubActions = process.env.GITHUB_ACTIONS === "true";
 
 // Configuración base dependiendo de si se ejecuta localmente o en GitHub Actions
-const base = isGitHubActions ? '/deploy_vitest-gh-pages' : '/'
+const base = isGitHubActions ? "/deploy_vitest-gh-pages" : "/";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: base,
   plugins: [react()],
-})
+});
