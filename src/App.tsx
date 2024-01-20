5c58832f66442202d46c6708de42dc2e48faa941
Jaen Figueroa <jaenfigueroa03042000@gmail.com>
2024-01-20 06:32:24 -0500

    chore: finish

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import img1 from 'bot.jpg'
+import img2 from '/bot.jpg'
+// import img3 from './bot.jpg'
+import img4 from '/public/bot.jpg'
+import img5 from '../public/bot.jpg'
+/*  */
+// import img6 from 'empresa/bot.jpg'
+import img7 from '/empresa/bot.jpg'
+// import img8 from './empresa/bot.jpg'
+import img9 from '/public/empresa/bot.jpg'
+import img10 from '../public/empresa/bot.jpg'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  /* 
+  EN LOCAL --> SOLO LAS QDESCOMENTADAS FUNCIONAN
+  EN DEPLOY --> SOLO FUNCIONAN LAS QUE DICEN QUE "FUNCIONA EN DEPLOY"
+  */
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {/* <img src={img1} alt='-1' /> */}
+      <img src={img2} alt='-2' /> {/* FUNCIONA EN DEPLOY */}
+      {/* <img src={img3} alt='-3' /> */}
+      <img src={img4} alt='-4' /> {/* FUNCIONA EN DEPLOY */}
+      <img src={img5} alt='-5' /> {/* FUNCIONA EN DEPLOY */}
+      {/*<img src={img6} alt='-6' /> */}
+      <img src={img7} alt='-7' /> {/* FUNCIONA EN DEPLOY */}
+      {/* <img src={img8} alt='-8' /> */}
+      <img src={img9} alt='-9' /> {/* FUNCIONA EN DEPLOY */}
+      <img src={img10} alt='-10' /> {/* FUNCIONA EN DEPLOY */}
+      {/*  */}
+      <img src='bot.jpg' alt='1' /> {/* FUNCIONA EN DEPLOY */}
+      <img src='/bot.jpg' alt='2' />
+      <img src='./bot.jpg' alt='3' /> {/* FUNCIONA EN DEPLOY */}
+      <img src='/public/bot.jpg' alt='4' />
+      {/*  */}
+      <img src='empresa/bot.jpg' alt='5' /> {/* FUNCIONA EN DEPLOY */}
+      <img src='/empresa/bot.jpg' alt='6' />
+      <img src='./empresa/bot.jpg' alt='7' /> {/* FUNCIONA EN DEPLOY */}
+      <img src='/public/empresa/bot.jpg' alt='8' />
     </>
   )
 }
